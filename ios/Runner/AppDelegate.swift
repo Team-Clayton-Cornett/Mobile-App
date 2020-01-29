1a61d359a1efd298dda6e16815a5bf121560a26d
ryanfilkins97 <rmfvg5@mail.missouri.edu>
2020-01-29 15:32:06 -0600

    Added Google Maps API dependency

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,15 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    
+    if let path = Bundle.main.path(forResource: "Keys", ofType: "plist") {
+        let keys = NSDictionary(contentsOfFile: path)
+        
+        if let googleMapsApiKey = keys?.value(forKey: "google_maps_api_key") as? String {
+            GMSServices.provideAPIKey(googleMapsApiKey)
+        }
+    }
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
