f60f319fef7d8aa0fa9575d5acbe0359dabdf13a
ryanfilkins97 <rmfvg5@mail.missouri.edu>
2020-02-11 17:23:33 -0600

    Created garage list card widget

---
+++ lib/components/probabilityIndicator.dart
@@ -0,0 +1,40 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class ProbabilityIndicator extends StatelessWidget {
+  final double diameter;
+  final double probability;
+
+  double greenPercent;
+  double redPercent;
+
+  ProbabilityIndicator({
+    @required this.diameter,
+    @required this.probability
+  }) {
+    // Based on https://stackoverflow.com/questions/25007/conditional-formatting-percentage-to-color-conversion
+    greenPercent = min(2.0 - (probability * 2), 1.0);
+    redPercent = min(probability * 2, 1.0);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: 2.0,
+      borderRadius: BorderRadius.circular(diameter / 2),
+      child: Container(
+        width: diameter,
+        height: diameter,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Color.fromARGB(255, (redPercent * 255).round(), (greenPercent * 255).round(), 0),
+          border: Border.all(
+              color: Colors.white,
+              width: 3.0
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
+import 'dart:math';
 
+import 'package:capstone_app/components/garageCard.dart';
 import 'package:capstone_app/components/handle.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -23,8 +25,14 @@ class _HomePageState extends State<HomePage> {
 
   bool _bottomSheetExpanded = false;
 
+  // TODO: Remove when actual probabilities are available
+  Random random = Random();
+  List<double> probabilities;
+
   // TODO: Populate markers with LatLng coordinates of garages
-  _HomePageState();
+  _HomePageState() {
+    probabilities = List.generate(50, (index) {return random.nextDouble();});
+  }
 
   // Called when the Google Map has been successfully created
   void _onMapCreated(GoogleMapController controller) {
@@ -59,7 +67,9 @@ class _HomePageState extends State<HomePage> {
         title: Text("Home"),
       ),
       body: SlidingUpPanel(
-        maxHeight: MediaQuery.of(context).size.height,
+        // Max height needs to be set to the total height of the page - the height of the toolbar - a bit of padding
+        // If this is not set correctly the garage list will not be able to scroll to the bottom
+        maxHeight: MediaQuery.of(context).size.height - kToolbarHeight - 20,
         borderRadius: sheetRadius,
         onPanelSlide: _onPanelSlide,
         body: GoogleMap(
@@ -83,8 +93,9 @@ class _HomePageState extends State<HomePage> {
                     controller: scrollController,
                     itemCount: 25,
                     itemBuilder: (BuildContext context, int index) {
-                      return ListTile(
-                        title: Text("Garage #$index"),
+                      return GarageCard(
+                        name: "Garage $index",
+                        ticketProbability: probabilities[index],
                       );
                     },
                   ),
