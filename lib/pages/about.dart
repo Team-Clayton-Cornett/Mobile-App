620402bba6cb43d8832f3b93a7550085a215a6e3
bmsc7d <bmsc7d@mail.missouri.edu>
2020-04-26 21:27:13 -0500

    Fixed AppBar on about page

--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -1,20 +1,21 @@
 import 'package:capstone_app/main.dart';
 import 'package:flutter/material.dart';
 import 'package:capstone_app/style/appTheme.dart';
+import 'package:capstone_app/services/auth.dart';
 
 
 class AboutPage extends StatelessWidget {
+  GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          tooltip: 'Navigation menu',
-          onPressed: null,
-        ),
         title: Text('About'),
-      ),
+        centerTitle: false,
+        ),
       // body is the majority of the screen.
       body: SafeArea(
         child: Column(
@@ -38,6 +39,66 @@ class AboutPage extends StatelessWidget {
         ],
     ),
       ),
+
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'App Name',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Home'),
+              onTap: (){
+                Navigator.of(context).pushReplacementNamed('/home');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.report),
+              title: Text('Report'),
+            ),
+            ListTile(
+              leading: Icon(Icons.history),
+              title: Text('History'),
+              onTap: () {
+                Navigator.pushNamedAndRemoveUntil(context, "/history", (r) => false);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('Account'),
+            ),
+            ListTile(
+              leading: Icon(Icons.help),
+              title: Text('About'),
+              onTap: () {
+                Navigator.pushNamedAndRemoveUntil(context, "/about", (r) => false);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.directions_run),
+              title: Text('Sign Out'),
+              onTap: () {
+                AuthService appAuth = new AuthService();
+                appAuth.logout();
+
+                Navigator.pushNamedAndRemoveUntil(context, "/login", (r) => false);
+              },
+            ),
+          ],
+        ),
+      ),
+
     );
   }
 }
